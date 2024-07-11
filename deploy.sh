ae7dd89735beda9e44ddaf43ea2291fadba4ce15
Kristijan Todorovic <kristijan.todorovic@hotmail.com>
2024-07-11 20:43:27 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:mellwax/punoletstvo.git main:gh-pages
+git push -f git@github.com:mellwax/punoletstvo.git master:gh-pages
 
 cd -
